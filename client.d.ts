2ace2582420fc7ca7af99a502ca2331075cba23c
EGOIST <hi@egoist.dev>
2024-12-23 15:41:46 +0800

    cleanup

--- client.d.ts
+++ client.d.ts
@@ -3,8 +3,3 @@ type MaybePromise<T> = T | Promise<T>
 declare const compileTime: <T>(
   fn: MaybePromise<T> | (() => MaybePromise<T>),
 ) => T
-
-declare const compileTimeFn =
-  <T>(fn: () => MaybePromise<T>) =>
-  () =>
-    T
